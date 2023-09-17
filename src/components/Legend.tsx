32079d53ee614a48608353f677608d2df946ca8c
Steven <stzheng716@gmail.com>
2023-09-16 17:57:45 -0700

    adjust legend

--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -4,7 +4,7 @@ import legend from "../assets/images/marker_legend.png";
 function Legend() {
     return (
         <div className="absolute bottom-8 left-2 bg-transparent rounded-md">
-            <img src={legend} alt="map legend" className="max-w-[20%]"/>
+            <img src={legend} alt="map legend" className="max-w-[30%]"/>
         </div>
     )
 }
