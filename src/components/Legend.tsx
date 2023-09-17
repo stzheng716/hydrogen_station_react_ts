1208d705b9e54feffc261b448ecea482969f5f31
Steven <stzheng716@gmail.com>
2023-09-16 17:41:54 -0700

    adjust legend

---
+++ src/components/Legend.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import legend from "../assets/images/marker_legend.png";
+
+function Legend() {
+    return (
+        <div className="absolute bottom-8 left-2 bg-transparent rounded-md">
+            <img src={legend} alt="map legend" className="max-w-[20%]"/>
+        </div>
+    )
+}
+
+export default Legend
